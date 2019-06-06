9a0cc9c218f3dd3d56197c50aa94c9a435367457
Tadashimi <tadashimi777@gmail.com>
2019-06-07 00:50:04 +0600

    Change synchronized usage

--- src/main/java/kz/nowebsite/repository/NotesRepositoryImpl.java
+++ src/main/java/kz/nowebsite/repository/NotesRepositoryImpl.java
@@ -3,12 +3,13 @@ package kz.nowebsite.repository;
 import kz.nowebsite.domain.Note;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
 public class NotesRepositoryImpl implements NotesRepository {
 
-    private List<Note> notes = new ArrayList<>();
+    private List<Note> notes = Collections.synchronizedList(new ArrayList<>());
 
     public NotesRepositoryImpl() {
         Note note = new Note("Текст1", "Заголовок");
@@ -31,38 +32,46 @@ public class NotesRepositoryImpl implements NotesRepository {
     }
 
     @Override
-    public synchronized Note getById(UUID id) {
-        for (Note n : notes) {
-            if (n.getId().equals(id)) return new Note(n);
+    public Note getById(UUID id) {
+        synchronized (notes) {
+            for (Note n : notes) {
+                if (n.getId().equals(id)) return new Note(n);
+            }
         }
         return null;
     }
 
     @Override
-    public synchronized List<Note> getSearchResult(String subString) {
+    public List<Note> getSearchResult(String subString) {
         String substringInLowerCase = subString.toLowerCase();
         List<Note> result = new ArrayList<>();
-        for (Note currentNote : notes) {
-            if (currentNote.getTitle().toLowerCase().contains(substringInLowerCase) || currentNote.getText().toLowerCase().contains(subString))
-                result.add(new Note(currentNote));
+        synchronized (notes) {
+            for (Note currentNote : notes) {
+                if (currentNote.getTitle().toLowerCase().contains(substringInLowerCase) || currentNote.getText().toLowerCase().contains(subString))
+                    result.add(new Note(currentNote));
+            }
         }
         return result;
     }
 
     @Override
-    public synchronized void changeNote(Note note) {
-        for (int i = 0; i < notes.size(); i++) {
-            Note currentNote = notes.get(i);
-            if (currentNote.getId().equals(note.getId())) {
-                notes.set(i, new Note(note));
-                break;
+    public void changeNote(Note note) {
+        synchronized (notes) {
+            for (int i = 0; i < notes.size(); i++) {
+                Note currentNote = notes.get(i);
+                if (currentNote.getId().equals(note.getId())) {
+                    notes.set(i, new Note(note));
+                    break;
+                }
             }
         }
     }
 
     private synchronized List<Note> cloneNotes() {
         List<Note> clone = new ArrayList<>(notes.size());
-        for (Note note : notes) clone.add(new Note(note));
+        synchronized (notes) {
+            for (Note note : notes) clone.add(new Note(note));
+        }
         return clone;
     }
 
